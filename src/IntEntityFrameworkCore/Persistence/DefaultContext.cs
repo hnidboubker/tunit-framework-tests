6f5a5b88a158b181e0345f62ceb47906b5ba7ae2
Houssine <nidboubkerhoussine@live.fr>
2026-08-14 11:55:44 +0200

    Updates: 2026-08-14T09:55:41.748Z

---
+++ src/IntEntityFrameworkCore/Persistence/DefaultContext.cs
@@ -0,0 +1,12 @@
+﻿using IntCore.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntEntityFrameworkCore.Persistence
+{
+    public class DefaultContext 
+    {
+    }
+}
