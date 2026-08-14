aad6d4b5555374f11c4a7c271fe04d91143626c9
Houssine <nidboubkerhoussine@live.fr>
2026-08-14 12:07:53 +0200

    Updates: 2026-08-14T10:07:51.030Z

--- src/IntEntityFrameworkCore/Persistence/DefaultContext.cs
+++ src/IntEntityFrameworkCore/Persistence/DefaultContext.cs
@@ -1,12 +1,21 @@
-﻿using IntCore.Entities;
+﻿using IntCore.Models.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace IntEntityFrameworkCore.Persistence
 {
-    public class DefaultContext 
+    public class DefaultContext : IdentityDbContext<User, Role, int>
     {
+        public DefaultContext(DbContextOptions<DefaultContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configurações adicionais das entidades, se necessário.
+        }
     }
 }
